fa6c366e8f241c016ff6e70c0dd1381abf52841d
risingsunomi <vincentcastro@gmail.com>
2025-06-17 04:12:39 -0800

    revert shard download changes, ignore safetensor json map not found errors

--- xotorch/download/new_shard_download.py
+++ xotorch/download/new_shard_download.py
@@ -152,20 +152,11 @@ async def _download_file(repo_id: str, revision: str, path: str, target_dir: Pat
     n_read = resume_byte_pos or 0
     async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=1800, connect=60, sock_read=1800, sock_connect=60)) as session:
       async with session.get(url, headers=headers, timeout=aiohttp.ClientTimeout(total=1800, connect=60, sock_read=1800, sock_connect=60)) as r:
-        skip_file = False
-        if r.status == 404 and "model.safetensors.index.json" not in path:
-          raise FileNotFoundError(f"File not found: {url}")
-        else:
-          skip_file = True
-
-        if "model.safetensors.index.json" not in path:
-          assert r.status in [200, 206], f"Failed to download {path} from {url}: {r.status}"
-        elif r.status not in [200, 206]:
-          skip_file = True
         
-        if not skip_file:
-          async with aiofiles.open(partial_path, 'ab' if resume_byte_pos else 'wb') as f:
-            while chunk := await r.content.read(8 * 1024 * 1024): on_progress(n_read := n_read + await f.write(chunk), length)
+        if r.status == 404: raise FileNotFoundError(f"File not found: {url}")
+        assert r.status in [200, 206], f"Failed to download {path} from {url}: {r.status}"
+        async with aiofiles.open(partial_path, 'ab' if resume_byte_pos else 'wb') as f:
+          while chunk := await r.content.read(8 * 1024 * 1024): on_progress(n_read := n_read + await f.write(chunk), length)
 
   final_hash = await calc_hash(partial_path, type="sha256" if len(remote_hash) == 64 else "sha1")
   integrity = final_hash == remote_hash
